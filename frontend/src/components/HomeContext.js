e03f7cd58b1f82a95041dda2418d44037cf4d0ba
joshua-stacks <vasquez.joshua1115@gmail.com>
2022-08-15 01:02:16 -0400

    transfer to main computer

--- frontend/src/components/HomeContext.js
+++ frontend/src/components/HomeContext.js
@@ -1,50 +1,47 @@
-import { createContext, useEffect,useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const HomeContext = createContext();
 
-export const HomeProvider = ({children}) => {
-const [facts,setFacts] = useState()
-const [status, setStatus] = useState(false);
-const [city,setCity] = useState(null)
-const [load, setLoad] = useState(false);
+export const HomeProvider = ({ children }) => {
+  const [facts, setFacts] = useState();
+  const [status, setStatus] = useState(false);
+  const [city, setCity] = useState(null);
+  const [load, setLoad] = useState(false);
 
-    useEffect(()=>{
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("/cities");
+      const json = await data.json();
+      setLoad(true);
+      setCity(json.cities);
+      return json;
+    };
+    fetchData().catch((err) => {
+      console.log(err);
+    });
+  }, []);
+  useEffect(() => {
+    const fetchFacts = async () => {
+      const data = await fetch("/facts");
+      const json = await data.json();
+      setStatus(true);
+      setFacts(json);
+      return json;
+    };
+    fetchFacts().catch((err) => {
+      console.log(err);
+    });
+  }, []);
 
-        const fetchData = async () => {
-        const data = await fetch("/cities");
-        const json = await data.json();
-        setLoad(true)
-        setCity(json.cities)
-        return json
-      };
-      fetchData().catch((err) => {
-        console.log(err)
-      });    
-
-},[])
-useEffect(()=>{
-              const fetchFacts = async () => {
-        const data = await fetch("/facts");
-        const json = await data.json();
-        setStatus(true);
-        setFacts(json);
-        return json
-      };
-      fetchFacts().catch((err) => {
-        console.log(err)
-      }); 
-},[])
-
-if (load === false ) {
+  if (load === false) {
     return <>loading</>;
   }
-  if (status === false ) {
+  if (status === false) {
     return <>loading</>;
   }
   return (
-    <HomeContext.Provider value={{city,facts,load,status}}>
-        {children}
+    <HomeContext.Provider value={{ city, facts, load, status }}>
+      {children}
     </HomeContext.Provider>
-)
-
-}
+  );
+};
--- frontend/src/components/Watch.js
+++ frontend/src/components/Watch.js
@@ -1,4 +1,55 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api";
+
 const Watch = () => {
-  return <>the product page</>;
+  const [watch, setWatch] = useState();
+  const [status, setStatus] = useState(false);
+  const [gps, setGps] = useState();
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE,
+  });
+  const prms = useParams().id;
+
+  useEffect(() => {
+    const fetchFacts = async () => {
+      const data = await fetch(`/watch/${prms}`);
+      const json = await data.json();
+      setStatus(true);
+      setWatch(json);
+      return json;
+    };
+    fetchFacts().catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
+  if (!isLoaded) {
+    return <>loading</>;
+  }
+  if (status === false) {
+    return <>loading</>;
+  }
+  const center = { lat: 45.56951, lng: -73.75087 };
+  return (
+    <>
+      <div>{watch.watch.watchname}</div>
+      <img src={watch.watch.imageSrc} style={{ width: "20%" }} />
+      <div>{watch.price}</div>
+      <Div>
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+        />
+      </Div>
+    </>
+  );
 };
+const Div = styled.div`
+  border: solid;
+  width: 40%;
+  height: 40%;
+`;
 export default Watch;
